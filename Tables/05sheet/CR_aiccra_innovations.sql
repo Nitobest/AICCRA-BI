bfb3b2695121ebb578a2d324680fee85b2c2acb1
ccastiblancoCGIAR <c.castiblanco@cgiar.org>
2023-04-11 09:34:34 -0500

    Create Innovations ETL
    create ETL to AICCRA innovations

---
+++ Tables/05sheet/CR_aiccra_innovations.sql
@@ -0,0 +1,37 @@
+drop table if exists aiccra_innovations;
+
+CREATE TABLE aiccra_innovations
+(
+ innovation_id bigint
+, link_innovation_id text
+, innovation_year int
+, innovation_title text
+, innovation_stage text
+, innovation_stage_definition text
+, innovation_type text
+, geographic_scope text
+, region text
+, country text
+, gender_relevance text
+, gender_explanation_evidence text
+, youth_relevance text
+, youth_explanation_evidence text
+, indicator_acronym text
+, indicator_id bigint
+, indicator_name text
+, institution_name text
+, institution_role text
+, center_name text
+, contributor_name text
+, indicator_pk varchar(50)
+, contribution_pk varchar(255)
+, cluster_id bigint
+, cluster_owner_type text
+, cluster_owner_id bigint
+, cluster_acronym text
+, cluster_owner_acronym text
+, updated_date datetime
+, status text
+, cluster_title text
+)
+;
